4fe58e4e0379aa7cf01702e9802b122fedd24f14
Brian Staruk <brian@staruk.net>
2022-08-24 09:20:50 -0400

    applying fixes for eslint & stylelint errors/warnings

--- src/data/app/saga.js
+++ src/data/app/saga.js
@@ -3,12 +3,10 @@ import { all, put, takeLatest } from 'redux-saga/effects';
 import { actions } from './slice';
 
 export function* setCountSaga({ payload }) {
-  console.log('setCountSaga', payload);
+  console.log('setCountSaga', payload); // eslint-disable-line no-console
   yield put(actions.setCountSuccess(payload));
 }
 
 export function* appSaga() {
-  yield all([
-    takeLatest(actions.setCount.type, setCountSaga),
-  ]);
+  yield all([takeLatest(actions.setCount.type, setCountSaga)]);
 }
