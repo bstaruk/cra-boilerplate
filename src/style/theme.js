17c869a011b081c0adfc858578dbadb8a469ca92
Brian Staruk <brian@staruk.net>
2022-08-24 07:47:00 -0400

    styled-components init

---
+++ src/style/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  /* Colors */
+  color: {
+    black: '#000',
+    white: '#fff',
+  },
+
+  /* Font Families */
+  font: {
+    sans: 'sans-serif',
+    serif: 'serif',
+  },
+};
+
+export default theme;
